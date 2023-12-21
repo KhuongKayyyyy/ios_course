a2b0026e1a2c524cf1c7117be5927d63ed1b6fc4
KhuongKayyyyy <zzkhngzz@gmail.com>
2023-12-21 22:29:17 +0700

    i am rich app

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Khương Kayyy on 21/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
