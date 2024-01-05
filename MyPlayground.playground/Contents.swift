c5b70751c4c3b5e72a352f6d72bebad96379a81e
KhuongKayyyyy <zzkhngzz@gmail.com>
2024-01-05 23:18:23 +0700

    quizzer

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,5 +1,21 @@
-func loveCalculator(){
-    let loveScore = Int.random(in: 0...100)
+struct Town{
+    let name: String
+    var citizens :[String]
+    var resource :[String:Int]
+    
+    init(name: String, citizens: [String], resource: [String:Int]){
+        self.name = name
+        self.citizens = citizens
+        self.resource = resource
+    }
+    func fortify(){
+        print("Defend increased")
+    }
 }
 
-print(loveCalculator())
+var anotherTown = Town(name: "Nameless island", citizens: ["Khuong","Thanh"], resource: ["Coconuts":100])
+anotherTown.citizens.append("Tom Hanks")
+print(anotherTown.citizens)
+var ghostTown = Town(name:"Ghost McGhostface",citizens: [],resource: ["TumbleWeed":1])
+ghostTown.fortify()
+print(ghostTown.name)
